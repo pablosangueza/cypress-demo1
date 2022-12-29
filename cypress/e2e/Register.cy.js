3e27ace9ecde44b4c510965a76ec173e7ef4b67c
Pablo Sangueza <pablo.sangueza@parkmobile.io>
2022-12-28 22:49:10 -0400

    Register test case added

---
+++ cypress/e2e/Register.cy.js
@@ -0,0 +1,12 @@
+describe('Test Register workflow', () => {
+    it('Should show a message when the user is not at database', () => {
+      cy.visit('https://buggy.justtestit.org/')
+  
+      cy.get('[name="login"]').type('fakeUserPablo');
+      cy.get('[name="password"]').type('fakePass');
+      cy.get('[type="submit"]').click();
+  
+      cy.contains('Invalid username/password');
+  
+    })
+  })
